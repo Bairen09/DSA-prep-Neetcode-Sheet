b3575d98799428207665c94233ceeb75abe35019
Bairan Chakraborty <159531175+Bairen09@users.noreply.github.com>
2026-03-31 01:31:22 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>map= new HashMap<>();
+        
+        for(String s:strs){
+            int[]num= new int[26];
+            for(char c:s.toCharArray()){
+                num[c-'a']++;
+            }
+            StringBuilder key= new StringBuilder();
+            for(int i:num){
+                key.append(i).append("#");
+            }
+            String k=key.toString();
+            if(!map.containsKey(k)){
+                map.put(k,new ArrayList<>());
+            }
+            map.get(k).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
